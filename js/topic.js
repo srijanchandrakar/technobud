ef9daedff82dad3404c4b40ccd7ab0b0ac9db9d0
srijanchandrakar <srijanchandrakar10@gmail.com>
2024-08-22 18:26:04 +0530

    feat: js file update

--- js/topic.js
+++ js/topic.js
@@ -14,6 +14,56 @@ const afterimage= document.getElementById('afterimage')
 
 if(state==='website'){
     console.log('***** here')
+    firstele.innerText='website'
+    secondele.innerText='website development'
+    introduction.innerText="Introduction to Website development"
+    detail1.innerText="So how can you stand out, do something unique and interesting, build an online business, and get paid enough to change life. Please visit TemplateMo website to download free website templates."
+    detail2.innerText="There are so many ways to make money online. Below are several platforms you can use to find success. Keep in mind that there is no one path everyone can take. If that were the case, everyone would have a million dollars."
+    quote.innerText="Grow you bussiness by digitalizing your product/service"
+    afterimage.innerText="Most people start with freelancing skills they already have as a side hustle to build up income. This extra cash can be used for a vacation, to boost up savings, investing, build business."
+}
+else if(state==='software'){
+    console.log('***** here')
+    firstele.innerText='webpage'
+    secondele.innerText='website development'
+    introduction.innerText="Introduction to Website development"
+    detail1.innerText="So how can you stand out, do something unique and interesting, build an online business, and get paid enough to change life. Please visit TemplateMo website to download free website templates."
+    detail2.innerText="There are so many ways to make money online. Below are several platforms you can use to find success. Keep in mind that there is no one path everyone can take. If that were the case, everyone would have a million dollars."
+    quote.innerText="Grow you bussiness by digitalizing your product/service"
+    afterimage.innerText="Most people start with freelancing skills they already have as a side hustle to build up income. This extra cash can be used for a vacation, to boost up savings, investing, build business."
+}
+else if(state==='mobile'){
+    console.log('***** here')
+    firstele.innerText='webpage'
+    secondele.innerText='website development'
+    introduction.innerText="Introduction to Website development"
+    detail1.innerText="So how can you stand out, do something unique and interesting, build an online business, and get paid enough to change life. Please visit TemplateMo website to download free website templates."
+    detail2.innerText="There are so many ways to make money online. Below are several platforms you can use to find success. Keep in mind that there is no one path everyone can take. If that were the case, everyone would have a million dollars."
+    quote.innerText="Grow you bussiness by digitalizing your product/service"
+    afterimage.innerText="Most people start with freelancing skills they already have as a side hustle to build up income. This extra cash can be used for a vacation, to boost up savings, investing, build business."
+}
+else if(state==='saas'){
+    console.log('***** here')
+    firstele.innerText='webpage'
+    secondele.innerText='website development'
+    introduction.innerText="Introduction to Website development"
+    detail1.innerText="So how can you stand out, do something unique and interesting, build an online business, and get paid enough to change life. Please visit TemplateMo website to download free website templates."
+    detail2.innerText="There are so many ways to make money online. Below are several platforms you can use to find success. Keep in mind that there is no one path everyone can take. If that were the case, everyone would have a million dollars."
+    quote.innerText="Grow you bussiness by digitalizing your product/service"
+    afterimage.innerText="Most people start with freelancing skills they already have as a side hustle to build up income. This extra cash can be used for a vacation, to boost up savings, investing, build business."
+}
+else if(state==='frontend'){
+    console.log('***** here')
+    firstele.innerText='webpage'
+    secondele.innerText='website development'
+    introduction.innerText="Introduction to Website development"
+    detail1.innerText="So how can you stand out, do something unique and interesting, build an online business, and get paid enough to change life. Please visit TemplateMo website to download free website templates."
+    detail2.innerText="There are so many ways to make money online. Below are several platforms you can use to find success. Keep in mind that there is no one path everyone can take. If that were the case, everyone would have a million dollars."
+    quote.innerText="Grow you bussiness by digitalizing your product/service"
+    afterimage.innerText="Most people start with freelancing skills they already have as a side hustle to build up income. This extra cash can be used for a vacation, to boost up savings, investing, build business."
+}
+else if(state==='backend'){
+    console.log('***** here')
     firstele.innerText='webpage'
     secondele.innerText='website development'
     introduction.innerText="Introduction to Website development"
